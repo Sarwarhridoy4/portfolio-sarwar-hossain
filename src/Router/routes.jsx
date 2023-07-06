1aea98fb6ce4853cda9d13975aafb2583413a649
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-07-06 10:09:34 +0600

    nav-bar added with layout

--- src/Router/routes.jsx
+++ src/Router/routes.jsx
@@ -2,18 +2,28 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main/Main";
 import Error from "../Pages/Error/Error";
 import Home from "../Pages/Home/Home";
+import Projects from "../Pages/Preojects/Projects";
+import About from "../Pages/About/About";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main />,
-        errorElement: <Error />,
-        children: [
-            {
-                path: '/',
-                element: <Home/>
-            }
-        ]
-    }
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
 ]);
 export default router;
