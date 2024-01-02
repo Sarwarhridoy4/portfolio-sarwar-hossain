386c485991564e6668f587d6a6c6349455d59ab7
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2024-01-02 20:17:25 +0600

    hide x axis

--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -4,7 +4,7 @@ import Footer from "../../Pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
-    <div className='w-full'>
+    <div className='w-full overflow-x-hidden'>
       <Header />
       <Outlet />
       <Footer />
