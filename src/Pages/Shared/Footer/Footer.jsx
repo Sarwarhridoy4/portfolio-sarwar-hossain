221da3789e5c31294318f42fece60d77f2e93067
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2024-01-02 21:22:49 +0600

    add copyright in footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
+
 const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(null);
+
+  useEffect(() => {
+    const getCurrentYear = () => {
+      const year = new Date().getFullYear();
+      setCurrentYear(year);
+    };
+    getCurrentYear();
+  }, [currentYear]);
   return (
     <div className='grid grid-cols-12'>
-      <div className='col-span-full flex items-baseline justify-center mb-20 mt-10'>
+      <div className='col-span-full flex items-baseline justify-center my-8'>
         <p>&copy; Design and Developed By: Sarwar Hossain</p>
+      </div>
+      <div className='col-span-full flex items-baseline justify-center mb-20'>
+        <p>&copy; Copyright : {currentYear}</p>
       </div>
     </div>
   );
